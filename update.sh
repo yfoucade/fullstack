02df9babdcc16f7e3d1872ddf120caa70f1a7701
Yohan Foucade <yohan.foucade@gmail.com>
2025-03-25 05:44:05 +0100

    create update script

--- update.sh
+++ update.sh
@@ -1,18 +1,19 @@
-# Il faut avoir le fichier .env dans backend/
-cp backend/.env .
+# Il faut avoir le fichier .env la racine du repo
 rm -rf backend
 git clone git@github.com:fullstack-hy2020/part3-notes-backend.git backend
 cd backend
 git switch part4-9
 rm -rf .git
-cp .env backend/
 npm i
 cd ..
+cp .env backend/
 
 rm -rf frontend
 git clone git@github.com:fullstack-hy2020/part2-notes-frontend.git frontend
 cd frontend
-git switch part5-8
+git switch part5-7
 rm -rf .git
 npm i
+npm run build
 cd ..
+cp -r frontend/dist/ backend/
